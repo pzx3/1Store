89f41a84575335731adc67ffe795684a0f6232a5
pzx3 <55480017+pzx3@users.noreply.github.com>
2020-02-10 22:35:17 +0300

    style.cc

--- style.cc
+++ style.cc
@@ -1,4 +1,5 @@
-body
+
+    body
 {
     
     background:#2B2D2D;
@@ -46,3 +47,75 @@ body
     line-height: 2em;
     
 }
+    
+    
+    
+    
+    
+    
+    .credits-top {
+      font-size: 0.80em;
+      padding-right: 10px;
+      padding-left: 10px;
+      color: rgba(255, 255, 255, 0.5);
+
+    }
+    .download-btn {
+
+      color: white;
+      text-decoration:none;
+      background: #1896c65c;
+      box-shadow: 0px 0px 25px #1896c65c;
+      
+      padding-top: 13px !important;
+      padding-right: 40px !important;
+      padding-left: 40px !important;
+      padding-bottom: 13px !important;
+      
+      border-radius: 100px;
+      font-weight: bold;
+
+    }
+
+    .credits-bottom {
+      font-size: 0.9em;
+      padding-top: 100px !important;
+      padding-right: 10px;
+      padding-left: 10px;
+      color: rgba(255, 255, 255, 0.5);
+
+    }
+
+    .installation {
+      display: inline-block;
+      padding: 30px;
+      padding-left: 50px;
+      padding-top: 5px !important;
+      text-align: left;
+      opacity: 0.9;
+      font-size: 0.9em;
+    }
+
+
+    .dotted-list {
+      display: inline-block;
+      padding: 30px;
+      padding-left: 50px;
+      padding-top: 5px !important;
+      text-align: left;
+      opacity: 0.9;
+      list-style-type: circle;
+      font-size: 0.9em;
+    }
+
+    .warning-message {
+      font-size: 0.7em;
+
+      color: rgba(255, 255, 255, 0.8);
+    }
+
+  .twitter-link {
+      color: rgba(255, 255, 255, 0.5);
+        text-decoration: underline;
+    }
+ 
