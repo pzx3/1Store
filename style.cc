4934729c3611bc5d7d5e7a3f9971fa25c81ab90a
pzx3 <55480017+pzx3@users.noreply.github.com>
2020-02-10 21:51:34 +0300

    Update and rename Abdulmajeed 35 to style.cc

---
+++ style.cc
@@ -0,0 +1,48 @@
+body
+{
+    
+    background:#2B2D2D;
+    color: White
+    @import url('https://fonts.googleapis.com/css?family=Sriracha&display=swap');
+    
+    font-family: 'Sriracha', cursive;
+}
+
+.ss
+{
+    
+    width: 120px;
+    border-radius: 10px;
+
+}
+
+
+.ee
+{
+    width: 10px;
+    size: 1px;
+    margin-top: 50px;
+}
+
+
+.xx
+{
+    
+    
+    
+    
+}
+
+
+
+
+.bb
+{
+    color: #fff;
+    text-decoration: none;
+    border: #ccc 2px solid;
+    padding: 5px 15px;
+    border-radius: 10px;
+    line-height: 2em;
+    
+}
